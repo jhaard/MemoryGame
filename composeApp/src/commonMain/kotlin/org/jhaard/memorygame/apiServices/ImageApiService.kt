7bdb6e46042360e121b368c51c8acecc58bec863
Jörgen Hård <jorgen.hard@gmail.com>
2025-07-26 15:58:24 +0200

    Added the url back to the project and modified the proxy server to get the correct response to iOS.

--- composeApp/src/commonMain/kotlin/org/jhaard/memorygame/apiServices/ImageApiService.kt
+++ composeApp/src/commonMain/kotlin/org/jhaard/memorygame/apiServices/ImageApiService.kt
@@ -5,6 +5,7 @@ import io.ktor.client.call.body
 import io.ktor.client.request.get
 import org.jhaard.memorygame.httpClient.createHttpClient
 import org.jhaard.memorygame.models.iconImageModels.IconImageResponse
+import org.jhaard.memorygame.models.values.StringValues
 
 /**
  * Fetching icons to the memory tiles from proxy server url.
@@ -23,7 +24,7 @@ class ImageApiService(private val client: HttpClient = createHttpClient()) {
      */
     suspend fun getImageIcons(key: String): IconImageResponse {
 
-        val url: String = ""
+        val url: String = StringValues.BASE_URL + StringValues.QUERY_ADDITION + key
 
         return try {
             val response = client.get(url)
